014833e3067a53ac89f295fc8562aa65b18e92b0
Kuznetsov Sergey <monteg179@yandex.ru>
2024-04-16 07:09:11 +0300

    feat: добавил pandas-ta, сделал расчет rsi

--- rsi_bot/bybit.py
+++ rsi_bot/bybit.py
@@ -1,10 +1,16 @@
 import httpx
+from pandas import (
+    DataFrame,
+)
+from pandas_ta import (
+    rsi,
+)
 
 
 class Bybit:
 
     @staticmethod
-    async def get_kline(symbol: str, interval: str, limit: int):
+    async def get_kline(symbol: str, interval: str, limit: int = 200):
         async with httpx.AsyncClient() as client:
             response = await client.get(
                 url='https://api-testnet.bybit.com/v5/market/kline',
@@ -17,6 +23,14 @@ class Bybit:
             )
             if response.status_code == httpx.codes.OK:
                 return response.json()
+
+    @staticmethod
+    def get_kline_rsi(json: dict, length: int) -> float:
+        candles = json['result']['list']
+        data = DataFrame(
+            data={'close': [float(candle[4]) for candle in candles[::-1]]}
+        )
+        return rsi(data.close, length).iloc[-1]
 
     @staticmethod
     async def get_historical_volatility(
--- rsi_bot/main.py
+++ rsi_bot/main.py
@@ -34,32 +34,70 @@ def remove_job(name: str, queue: JobQueue) -> None:
         job.schedule_removal()
 
 
-async def rsi(context: ContextTypes.DEFAULT_TYPE) -> None:
-    await Bybit.get_kline('BTCUSD', '30', 10)
+def remove_all_jobs(queue: JobQueue) -> None:
+    for job in queue.jobs():
+        job.schedule_removal()
 
 
-async def volatility(context: ContextTypes.DEFAULT_TYPE) -> None:
-    await Bybit.get_historical_volatility('option', 'BTC', 30)
+async def rsi_job(context: ContextTypes.DEFAULT_TYPE) -> None:
+    data = context.job.data
+    json = await Bybit.get_kline(data['coin'], data['timeframe'])
+    if json:
+        rsi = Bybit.get_kline_rsi(json, data['rsi_length'])
+        await context.bot.send_message(
+            chat_id=context.job.chat_id,
+            text=f'rsi: {rsi:.2f}'
+        )
 
 
-async def poc(context: ContextTypes.DEFAULT_TYPE) -> None:
-    await Bybit.get_kline('BTCUSD', '30', 10)
+async def volatility_job(context: ContextTypes.DEFAULT_TYPE) -> None:
+    data = await Bybit.get_historical_volatility('option', 'BTC', 30)
+    if data:
+        await context.bot.send_message(
+            chat_id=context.job.chat_id,
+            text='volatility: '
+        )
+
+
+async def poc_job(context: ContextTypes.DEFAULT_TYPE) -> None:
+    data = await Bybit.get_kline('BTCUSD', '30', 10)
+    if data:
+        await context.bot.send_message(
+            chat_id=context.job.chat_id,
+            text='poc: '
+        )
 
 
 async def rsi_handler(
     update: Update,
     context: ContextTypes.DEFAULT_TYPE
 ) -> None:
-    chat_id = update.effective_message.chat_id
-    job_name = str(chat_id)
-    remove_job(job_name, context.job_queue)
-    context.job_queue.run_repeating(
-        callback=rsi,
-        first=30.0,
-        interval=30.0,
-        chat_id=chat_id,
-        name=job_name
-    )
+    chat_id = update.effective_chat.id
+    user_id = update.effective_user.id
+    try:
+        coin = context.args[0]
+        timeframe = context.args[1]
+        rsi_length = int(context.args[2])
+    except Exception:
+        await context.bot.send_message(
+            chat_id=chat_id,
+            text='Usage: /rsi <coin> <timeframe> <rsi length>'
+        )
+    else:
+        job_name = str(chat_id)
+        remove_job(job_name, context.job_queue)
+        context.job_queue.run_repeating(
+            callback=rsi_job,
+            interval=30.0,
+            chat_id=chat_id,
+            user_id=user_id,
+            data={
+                'coin': coin,
+                'timeframe': timeframe,
+                'rsi_length': rsi_length,
+            },
+            name=job_name
+        )
 
 
 async def volatility_handler(
@@ -70,9 +108,8 @@ async def volatility_handler(
     job_name = str(chat_id)
     remove_job(job_name, context.job_queue)
     context.job_queue.run_repeating(
-        callback=volatility,
-        first=30.0,
-        interval=30.0,
+        callback=volatility_job,
+        interval=60.0,
         chat_id=chat_id,
         name=job_name
     )
@@ -86,12 +123,18 @@ async def poc_handler(
     job_name = str(chat_id)
     remove_job(job_name, context.job_queue)
     context.job_queue.run_repeating(
-        callback=poc,
-        first=30.0,
-        interval=30.0,
+        callback=poc_job,
+        interval=60.0,
         chat_id=chat_id,
         name=job_name
     )
+
+
+async def stop_handler(
+    update: Update,
+    context: ContextTypes.DEFAULT_TYPE
+) -> None:
+    remove_all_jobs(context.job_queue)
 
 
 def main() -> None:
@@ -105,6 +148,9 @@ def main() -> None:
     )
     app.add_handler(
         CommandHandler('poc', poc_handler)
+    )
+    app.add_handler(
+        CommandHandler('stop', stop_handler)
     )
     app.run_polling(allowed_updates=Update.ALL_TYPES)
 
